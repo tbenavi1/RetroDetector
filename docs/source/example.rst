6a321116172ccd74c477aca520abcbf4e4be84fb
tbenavi1 <tbenavi1@jhmi.edu>
2022-06-09 14:47:38 -0400

    updates

--- docs/source/example.rst
+++ docs/source/example.rst
@@ -8,38 +8,67 @@ In this example, we run the entire RetroDetector pipeline on a *Drosophila melan
 
 .. code-block:: console
    
-   mkdir Reference
-   wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz -O Reference/ncbi_dmel.genomic.fna.gz
-   wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.gtf.gz -O Reference/ncbi_dmel.genomic.gtf.gz
-   wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_rna_from_genomic.fna.gz -O Reference/ncbi_dmel.rna_from_genomic.fna.gz
+   mkdir Downloads
+   wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz -O Downloads/ncbi_dmel.genomic.fna.gz
+   wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.gtf.gz -O Downloads/ncbi_dmel.genomic.gtf.gz
+   wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_rna_from_genomic.fna.gz -O Downloads/ncbi_dmel.rna_from_genomic.fna.gz
 
 The next step is to download and gzip the sequencing data. Make sure you have `sra-tools <https://github.com/ncbi/sra-tools>`_ installed. If you need more assistance please follow the instructions for `downloading <https://github.com/ncbi/sra-tools/wiki/01.-Downloading-SRA-Toolkit>`_, `installing <https://github.com/ncbi/sra-tools/wiki/02.-Installing-SRA-Toolkit>`_, and using `fasterq-dump <https://github.com/ncbi/sra-tools/wiki/08.-prefetch-and-fasterq-dump>`_.
 
 .. code-block:: console
 
-   mkdir FASTQS
    prefetch SRR10238607 --max-size 21G
-   fasterq-dump SRR10238607 --outdir FASTQS
-   gzip FASTQS/SRR10238607.fastq
+   fasterq-dump SRR10238607 --outdir Downloads
+   gzip Downloads/SRR10238607.fastq
    prefetch SRR10728584
-   fasterq-dump SRR10728584 --outdir FASTQS
-   gzip FASTQS/SRR10728584_1.fastq
-   gzip FASTQS/SRR10728584_2.fastq
+   fasterq-dump SRR10728584 --outdir Downloads
+   gzip Downloads/SRR10728584_1.fastq
+   gzip Downloads/SRR10728584_2.fastq
  
-The next step is to set up the configuration file config.yaml which tells RetroDetector where to find the necessary input files. For this example, you can download `config.yaml <https://raw.githubusercontent.com/tbenavi1/RetroDetector/main/example/config.yaml>`_ to your working directory, or you can copy the following code to a new file in your working directory called config.yaml:
+The next step is to set up the configuration file config.yaml which tells RetroDetector where to find the necessary input files. If RetroDetector was installed at /user/software/RetroDetector, you would copy config.yaml to your working directory with the following command:
 
 .. code-block:: console
 
-   ref:
-     ncbi_dmel:
-       genomic_fna: Reference/ncbi_dmel.genomic.fna.gz
-       genomic_gtf: Reference/ncbi_dmel.genomic.gtf.gz
-       rna_from_genomic: Reference/ncbi_dmel.rna_from_genomic.fna.gz
+   cp /users/software/RetroDetector/example/config.yaml .
 
-   fastqs:
-     short_paired_end:
-       ISO1:
-         1: [FASTQS/SRR10728584_1.fastq.gz, FASTQS/SRR10728584_2.fastq.gz]
-     long:
-       ISO1:
-         1: FASTQS/SRR10238607.fastq.gz
+Now, you need edit config.yaml to add the scripts directory location from your RetroDetector installation. For example, if RetroDetector was installed at /user/software/RetroDetector, you would edit the scripts directory line of config.yaml as follows:
+
+.. code-block:: console
+
+   scripts_directory: /user/software/RetroDetector/scripts
+
+Next, copy Snakefile from your RetroDetector installation to your working directory. For example, if RetroDetector was installed at /user/software/RetroDetector and you are in your working directory, you would run:
+
+.. code-block:: console
+
+   cp /user/software/RetroDetector/Snakefile .
+
+Now you are ready to run RetroDetector. Activate your snakemake environment, and perform a dry run.
+
+.. code-block:: console
+
+   conda activate snakemake
+   snakemake -n
+
+If everything looks good, specify the number of available cores and run RetroDetector. For example, if your machine has 32 available cores, you would run:
+
+.. code-block:: console
+
+   snakemake --cores 32
+
+Once the pipeline finishes running, the following files should be in the RESULTS folder:
+
+.. code-block:: console
+
+   RESULTS
+   ├── genome.fa
+   ├── genome.fa.amb
+   ├── genome.fa.ann
+   ├── genome.fa.bwt
+   ├── genome.fa.fai
+   ├── genome.fa.pac
+   ├── genome.fa.sa
+   └── samples
+       ├── A.fastq
+       ├── B.fastq
+       └── C.fastq
---
+++ scripts/get_transcript_junctions.py
@@ -0,0 +1,114 @@
+import gzip
+
+with gzip.open(snakemake.input[0], "rt") as input_transcript_file, open(snakemake.output[0], "w") as output_junction_file, open(snakemake.output[1], "w") as output_shortregions_txt_file, open(snakemake.output[2], "w") as output_coords_file, open(snakemake.output[3], "w") as output_introns_file, open(snakemake.output[4], "w") as output_longregions_txt_file:
+	for line in input_transcript_file:
+		if line.startswith(">"):
+			transcript = line.strip().split(" ")[0][1:]
+			if transcript.startswith("FB"):
+				ncbi_transcript = False
+			else:
+				assert transcript.startswith("lcl")
+				ncbi_transcript = True
+			qualifiers = line.strip().split(" ")
+			#don't include any partial transcripts, any transcripts with exceptions, or any ncRNA
+			#these may occur in the NCBI transcript files
+			if ncbi_transcript and any(qualifier.startswith("[partial=") or qualifier.startswith("[exception=") or qualifier.startswith("[ncRNA_class=") for qualifier in qualifiers):
+				continue
+			if not ncbi_transcript:
+				loc = line.strip().split(";")[1]
+				chrom = loc.split(":")[0].split("=")[1]
+				ranges = loc.split(":")[1]
+				for qualifier in qualifiers:
+					if qualifier.startswith("dbxref="):
+						items = qualifier[7:].split(",")
+						for item in items:
+							if item.startswith("FlyBase_Annotation_IDs:GE"):
+								GeneID = item.split(":")[1]
+			else:
+				chrom = transcript.split("|")[1].split("_")[0] + "_" + transcript.split("|")[1].split("_")[1]
+				for qualifier in qualifiers:
+					if qualifier.startswith("[location="):
+						ranges = qualifier.split("=")[1][:-1]
+					if qualifier.startswith("[db_xref="):
+						if "," in qualifier:
+							GeneID = qualifier.split(",")[1].split(":")[1][:-1]
+						else:
+							GeneID = qualifier.split(":")[1][:-1]
+			#if there are at least two exons for this transcript
+			if "join" in ranges:
+				#if this transcript occurs on the forward strand
+				if "complement" not in ranges:
+					positions = ranges[5:-1]
+				#else this transcript occurs on the reverse strand
+				else:
+					positions = ranges[16:-2]
+			#else there is only one exon for this transcript and we do not include
+			else:
+				continue
+			exons = positions.split(",")
+			num_exons = len(exons)
+			gene_start = exons[0].split("..")[0]
+			gene_stop = exons[-1].split("..")[-1]
+			output_coords_file.write(f"{transcript}\t{GeneID}\t{chrom}\t{gene_start}\t{gene_stop}\n")
+			startstops = []
+			skip_line = False
+			for exon in exons:
+				#some of the lines seem malformatted where the exon doesn't have a start and stop position, we skip these
+				try:
+					start, stop = exon.split("..")
+					start, stop = int(start), int(stop)
+					startstops.append((start, stop))
+				except:
+					skip_line = True
+			if skip_line:
+				continue
+			junctions = []
+			ref_junctions = []
+			ref_introns = []
+			current_length = 0
+			for i, (start, stop) in enumerate(startstops):
+				if i > 0:
+					ref_junctions.append(start - 1)
+					previous_stop = startstops[i-1][1]
+					ref_introns.append(f"{previous_stop+1}-{start-1}")
+				if i < num_exons - 1:
+					ref_junctions.append(stop)
+				dist = stop - start + 1
+				current_length += dist
+				#we append the 1-indexed position of the last base pair before the junction
+				junctions.append(current_length)
+			#we remove the last item, since this will be the 1-indexed position of the
+			#last base in the transcript but this is not a junction between two exons
+			junctions = junctions[:-1]
+			if ranges.startswith("complement"):
+				junctions = [current_length - junction for junction in reversed(junctions)]
+				ref_introns = reversed(ref_introns)
+			junctions = "\t".join(str(junction) for junction in junctions)
+			ref_introns = "\t".join(ref_intron for ref_intron in ref_introns)
+			output_junction_file.write(f"{transcript}\t{junctions}\n")
+			output_introns_file.write(f"{transcript}\t{ref_introns}\n")
+			for ref_junction in ref_junctions:
+				short_start = ref_junction - 74
+				short_stop = ref_junction + 75
+				output_shortregions_txt_file.write(f"{chrom}:{short_start}-{short_stop}\n")
+				short_start = ref_junction - 129
+				short_stop = ref_junction + 20
+				output_shortregions_txt_file.write(f"{chrom}:{short_start}-{short_stop}\n")
+				short_start = ref_junction - 19
+				short_stop = ref_junction + 130
+				output_shortregions_txt_file.write(f"{chrom}:{short_start}-{short_stop}\n")
+				short_start = ref_junction - 139
+				short_stop = ref_junction + 10
+				output_shortregions_txt_file.write(f"{chrom}:{short_start}-{short_stop}\n")
+				short_start = ref_junction - 9
+				short_stop = ref_junction + 140
+				output_shortregions_txt_file.write(f"{chrom}:{short_start}-{short_stop}\n")
+				long_start = max(ref_junction - 4999, 1)
+				long_stop = ref_junction + 5000
+				output_longregions_txt_file.write(f"{chrom}:{long_start}-{long_stop}\n")
+				long_start = max(ref_junction - 9899, 1)
+				long_stop = ref_junction + 100
+				output_longregions_txt_file.write(f"{chrom}:{long_start}-{long_stop}\n")
+				long_start = max(ref_junction - 99, 1)
+				long_stop = ref_junction + 9900
+				output_longregions_txt_file.write(f"{chrom}:{long_start}-{long_stop}\n")
